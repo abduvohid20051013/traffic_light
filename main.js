957f1e98bce910971d2b6496ca8d55a9ab3abb27
abduvohid <74610688+abduvohid20051013@users.noreply.github.com>
2021-04-21 14:21:05 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+let red = document.querySelector("#red")
+let yellow = document.querySelector("#yellow")
+let green = document.querySelector("#green")
+
+
+function changeLights() {
+    setTimeout(() => {
+        redLight()
+    }, 0)
+    setTimeout(() => {
+        yellowLight()
+    }, 2000)
+    setTimeout(() => {
+        greenLight()
+    }, 2800)
+    setTimeout(() => {
+        yellowLight()
+    }, 4800)
+}
+
+function redLight() {
+    red.classList.remove('redColor')
+    red.classList.add('red')
+    setTimeout(() => {
+        red.classList.remove('red')
+        red.classList.add('redColor')
+    }, 2000)
+}
+
+function yellowLight() {
+    yellow.classList.remove('yellowColor')
+    yellow.classList.add('yellow')
+    setTimeout(() => {
+        yellow.classList.remove('yellow')
+        yellow.classList.add('yellowColor')
+    }, 800)
+}
+
+function greenLight() {
+    green.classList.remove('greenColor')
+    green.classList.add('green')
+    setTimeout(() => {
+        green.classList.remove('green')
+        green.classList.add('greenColor')
+    }, 2000)
+}
+
+setInterval(changeLights, 5600)
